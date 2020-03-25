cf262ea81f228bdacfdce5cac84d57adb0fb1087
IKoshelev <koshelew@live.com>
2020-03-25 23:19:34 +0100

    initial setup done

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import "./index.less";
+
+
+function App() {
+    return <div>good</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
